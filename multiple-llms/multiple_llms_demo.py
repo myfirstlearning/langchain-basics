e8cb9723af49479bdc8c42d8fedca205f6ffba57
myfirstlearning <dheerajb412@gmail.com>
2025-10-26 02:25:06 -0500

    add chaining and usage of multiple llms

---
+++ multiple-llms/multiple_llms_demo.py
@@ -0,0 +1,60 @@
+import os
+
+from langchain_openai import ChatOpenAI
+from langchain_core.globals import set_debug
+import streamlit as st
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+
+# Enable debug mode to see the full traceback
+set_debug(True)
+
+OPEN_API_KEY = os.getenv("OPENAI_API_KEY")  # Ensure the OpenAI API key is set in your environment
+gpt_llm = ChatOpenAI(model="gpt-4o", api_key=OPEN_API_KEY)
+
+# This prompt is used to generate the title for the speech
+title_prompt = PromptTemplate(
+    input_variables=["topic"],
+    template="""You are an experienced speech writer.
+    You need to craft an impactful title for a speech on the following topic: {topic}
+    Answer exactly with one title.
+    """
+)
+
+GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
+google_gemini_llm = ChatGoogleGenerativeAI(model="gemini-2.5-pro",
+                             google_api_key=GEMINI_API_KEY)
+
+# This prompt is used to generate the speech
+speech_prompt = PromptTemplate(
+    input_variables=["title"],
+    template="""You need to write a powerful speech of 350 words on the following title: {title}
+    """
+)
+
+# LCEL -> Language Chain Expression Language
+# pipe operator (|) to create the chain with prompt_template | llm,
+# which is part of LangChain Expression Language (LCEL)
+# StrOutputParser() is used to parse the output of the LLM from first_chain
+# lambda function is used to extract the title from the response object returned by the StrOutputParser and assign it to the variable title
+# st.write is used to display the title in the Streamlit app
+# [1] selects the second element of the tuple (the title string)
+first_chain = title_prompt | gpt_llm | StrOutputParser() | (lambda title: (st.write(title), title)[1])
+# use multiple LLMs
+second_chain = speech_prompt | google_gemini_llm # use Google Gemini LLM
+
+# The final chain is the result of the first chain piped into the second chain
+final_chain = first_chain | second_chain
+
+
+st.title("Speech Generator")
+
+topic = st.text_input("Enter the topic: ")
+
+
+if topic:
+    with st.spinner(f"Generating speech on {topic}..."):
+         response = final_chain.invoke({ "topic":topic})
+    st.write(response.content)
